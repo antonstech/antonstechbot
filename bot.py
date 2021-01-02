c49ea20adec70e5e70a0d66d70c3e897882bf017
Anton_the_Dev <58666027+anton-the-dev@users.noreply.github.com>
2021-01-02 20:28:59 +0100

    Paar kleine Verbesserungen

--- bot.py
+++ bot.py
@@ -4,25 +4,28 @@ import discord.ext
 from discord.ext import commands
 import json
 
+
 # Wichs Codierung
 # ä=Ã¼
 # ö=Ã¶
 
-tokengesetzt = input("Ist dein Token gesetzt? (j/n) ")
-if tokengesetzt == ("j"):
-    print("Noice")
-else:
-    bottoken = {"token": input("Dein Bot Token: ")}
-    with open("config.json", "w") as f:
-        json.dump(bottoken, f)
+def start():
+    tokengesetzt = input("Ist dein Token gesetzt? (j/n) ")
+    if tokengesetzt == "j":
+        print("Noice")
+    else:
+        bottoken = {"token": input("Dein Bot Token: ")}
+        with open("config.json", "w") as f:
+            json.dump(bottoken, f)
 
-prefixgesetzt = input("Ist dein Bot Prefix gesetzt? (j/n) ")
-if prefixgesetzt == ("j"):
-    print("Noice")
-else:
-    botprefix = {"prefix": input("Dein Bot Prefix: ")}
-    with open("prefix.json", "w") as f:
-        json.dump(botprefix, f)
+    prefixgesetzt = input("Ist dein Bot Prefix gesetzt? (j/n) ")
+    if prefixgesetzt == "j":
+        print("Noice")
+    else:
+        botprefix = {"prefix": input("Dein Bot Prefix: ")}
+        with open("prefix.json", "w") as f:
+            json.dump(botprefix, f)
+
 
 with open('./prefix.json', 'r') as f:
     json_stuff = json.load(f)
@@ -30,10 +33,15 @@ with open('./prefix.json', 'r') as f:
 
 client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
+start()
+
 
 @client.event
 async def on_ready():
-    print("Yess der Bot läuft :) || Du bist eingeloggt als {0.user}".format(client))
+    print("Yess der bot läuft :)".format(client))
+    print("Du bist eingeloggt als {0.user} auf discord.py Version {1} auf den Servern: ".format(client, discord.__version__))
+    for guild in client.guilds:
+        print("-" + guild.name)
     client.loop.create_task(status_task())
 
 
@@ -63,7 +71,8 @@ async def benutzerinfo(ctx, member: discord.Member):
     for role in member.roles:
         if not role.is_default():
             rollen += '{} \r\n'.format(role.mention)
-    embed.add_field(name='Höchste Rolle', value=member.top_role.mention, inline=True)
+    embed.add_field(name='Höchste Rolle', value=member.top_role.mention, inline=True),
+    embed.add_field(name="Benutzer ID", value=member.id, inline=True),
     embed.set_thumbnail(url=member.avatar_url)
     embed.set_footer(text='Benutzerinfo')
     await ctx.send(embed=embed)
