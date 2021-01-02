445cf3d700704252adea7c9308d9c2d6a1f52085
Anton_the_Dev <58666027+anton-the-dev@users.noreply.github.com>
2021-01-02 20:43:43 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #Das ist ein Script welches automatisch den Bot installiert.
 sudo apt-get install -y python3-pip
-sudo apt-get install -y git
 pip3 install discord.py
-git clone https://github.com/antonstech/simplediscordbot
-cd simplediscordbot
 python3 main.py
